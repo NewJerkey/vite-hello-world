62fa2b8b79a1a3e5be5c15346dd4a3c20b333d8e
NewJerkey <122385464+NewJerkey@users.noreply.github.com>
2023-02-08 18:42:17 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,24 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div class="buttons">
+    <button class="copy">Duplicate</button>
+    <button class="changeBackground">Change Background</button>
+    <button class="changeTitle">Change Title</button>
+    <button id="delete">Delete
+      Last Instance</button>
+  </div>
+  <div class="card">
+    <h2 class="header">
+      Bryce Schneider
+    </h2>
+    <img alt="Schneider" src="https://cdn.discordapp.com/attachments/1062806966021402684/1063160172773654659/IMG_9420.png" />
+    <p class="paragraph">
+      This image depicts a strapping young Penn State student reacting to an insane moment in his favorite class: IST
+      256. He can not believe what he is seeing!
+    </p>
+    <button class="details">
+      Details
+    </button>
+  </div>
 </template>
 
 <script setup>
